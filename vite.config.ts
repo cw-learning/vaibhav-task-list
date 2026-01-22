0568d572a830e16487fcfbc7c052442165957ab7
vaibhav-cw <vaibhav.codewalnut@codewalnutlabs.com>
2026-01-22 19:01:36 +0530

    fix: addressing cw-pr-bot comments

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [    
-    tailwindcss(),
-    react(),
-  ],
+    tailwindcss(), react()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
